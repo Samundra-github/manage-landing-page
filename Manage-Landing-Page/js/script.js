99d4a3fda35e53cedfbab43ecafb1411a9f3d965
Samundra Bhandari <76542341+Samundra-github@users.noreply.github.com>
2022-06-02 13:53:34 +0545

    Add files via upload

--- Manage-Landing-Page/js/script.js
+++ Manage-Landing-Page/js/script.js
@@ -7,35 +7,24 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
+
+
 /*Slider*/
-var slideIndex = 1;
-    showSlides(slideIndex);
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
         
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
+      }
     }
-        
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-        
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {
-            slideIndex = 1
-        }    
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-              slides[i].style.display = "none";  
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " active";
-        
-    }
+  });
